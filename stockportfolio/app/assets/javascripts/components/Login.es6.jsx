1780f13f9f53fe89d50197101a46ad57595bceb5
Sam Glickman & Sid Ahmed Merzouk <pair+sglickman17.sid-ah@devbootcamp.com>
2017-02-20 16:30:27 -0500

    add refs to signup

--- stockportfolio/app/assets/javascripts/components/Login.es6.jsx
+++ stockportfolio/app/assets/javascripts/components/Login.es6.jsx
@@ -1,12 +1,28 @@
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    $.ajax({
+      url: 'sessions/login',
+      method: 'post'
+    }).done(response => {
+      console.log(response)
+      debugger;
+    })
+  }
+
   render() {
     return(
       <div className="login-form">
         <h1>Sign In:</h1>
-        <form className="" action="/sessions/login" method="post">
-          <input type="text" name="user[email]" value="" placeholder="Email@email.com"/><br/><br/>
-          <input type="text" name="user[password]" value="" placeholder="Password"/><br/><br/>
-          <input type="submit" name="" value="login up"/>
+        <form  onSubmit={this.handleSubmit}>
+          <input type="text" name="user[email]"  ref='email' placeholder="Email@email.com"/><br/><br/>
+          <input type="text" name="user[password]" ref='password' placeholder="Password"/><br/><br/>
+          <input type="submit" name="" value="Sign In"/>
         </form>
       </div>
     )
