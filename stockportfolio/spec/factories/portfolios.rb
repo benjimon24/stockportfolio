7b969235cd01abeb5b61f2b7d308cd7078672315
Benjamin Li & Joyce Au <pair+benjimon24.jawt94@devbootcamp.com>
2017-02-20 17:12:14 -0500

    fix stock show route

--- stockportfolio/spec/factories/portfolios.rb
+++ stockportfolio/spec/factories/portfolios.rb
@@ -1,6 +1,14 @@
 FactoryGirl.define do
   factory :portfolio do
-    
-  end
+    association :user, factory: :user
 
+    after(:create) do |portfolio|
+      name "portfolio_test"
+      buy_price 135.72
+      volume 1
+      symbol AAPL
+
+    end
+
+  end
 end
