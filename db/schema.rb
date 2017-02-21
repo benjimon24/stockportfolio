577a80c75871be3fd01bead60cd6d9f557be14d5
Kendy Ng <email": null>
2017-02-21 12:46:50 -0500

    refactor stocks users controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(version: 20170220160500) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name",      null: false
     t.string   "last_name",       null: false
-    t.string   "email",           null: false
     t.string   "password_digest", null: false
+    t.string   "email",           null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
