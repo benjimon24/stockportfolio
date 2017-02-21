dfda5ccc2cbbaeaee9ae6e53b90841221cd9f412
sid-ah <sidah_10@hotmail.com>
2017-02-21 09:02:08 -0500

    fix layout & routes

--- stockportfolio/config/routes.rb
+++ stockportfolio/config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  root 'portfolios#index'
+  get '/', to: 'users#index'
   get 'stocks/search/:name', to: 'stocks#search'
   get 'stocks/:symbol', to: 'stocks#show'
   resources :users, except: [:index, :destroy]
   resources :portfolios
   resources :stocks, except: [:index]
   get '/session/login', to: 'session#login', as: 'login'
-  post '/session/login', to: 'sessions#create'
+  post '/session/login', to: 'session#create'
   get '/session/logout', to: 'session#logout', as: 'logout'
-
 end
